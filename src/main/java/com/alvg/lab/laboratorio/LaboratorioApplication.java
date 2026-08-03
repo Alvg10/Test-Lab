07d49f5f8fe84f7d55ed02ceb604c2c979605948
AlVg10-2 <304672791+Testing-Labs-10@users.noreply.github.com>
2026-08-03 10:26:41 -0500

    Arregla error en LaboratorioApplication

--- src/main/java/com/alvg/lab/laboratorio/LaboratorioApplication.java
+++ src/main/java/com/alvg/lab/laboratorio/LaboratorioApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LaboratorioApplication {
 
 	public static void main(String[] args) {
+		System.out.println(" prueba de cambio de ramas");
 		SpringApplication.run(LaboratorioApplication.class, args);
 	}
 
